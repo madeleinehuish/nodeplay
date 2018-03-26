c5d995be3e2d4abcec7873fefac7c4921e0f3245
Madeleine Huish <madeleinehuish@gmail.com>
2018-03-25 18:48:41 -0700

    second

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({ hi: 'there'});
+	res.send({ arcane: 'refuge'});
 })
 
 const PORT = process.env.PORT || 5000;
 console.log('Listening on port ', PORT);
 
 app.listen(PORT);
+
+//heroku : https://arcane-refuge-44340.herokuapp.com/ | https://git.heroku.com/arcane-refuge-44340.git
