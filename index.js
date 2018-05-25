72b157683d5aea761664d5f795ce08745e00b38e
Madeleine Huish <madeleinehuish@gmail.com>
2018-05-25 13:14:55 -0700

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ arcane: 'refuge'});
-})
+//console.developers.google.com
+passport.use(new GoogleStrategy({
+		clientID : keys.googleClientID,
+		clientSecret : keys.googleClientSecret,
+		callbackURL : '/auth/google/callback' //this is a little different than the video
+	}, (accessToken, refreshToken, profile, done) => {
+		console.log('accessToken: ', accessToken);
+		console.log('refreshToken: ', refreshToken);
+		console.log('profile: ', profile);
+	})
+);
+
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope : ['profile', 'email']
+	})
+);
+
+app.get(
+	'/auth/google/callback',
+	passport.authenticate('google')
+);
 
 const PORT = process.env.PORT || 5000;
 console.log('Listening on port ', PORT);
