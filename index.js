7f1595eae9c3aa7f928fedb21e4c76a270e31224
Madeleine Huish <madeleinehuish@gmail.com>
2018-05-26 00:21:52 -0700

    set up mongo and mongoose

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+require('./models/User'); //this needs to be run first, before passport
 require('./services/passport');
 
-const PORT = process.env.PORT || 5000;
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
 require('./routes/authRoutes')(app);
 
+const PORT = process.env.PORT || 5000;
 console.log('Listening on port ', PORT);
 app.listen(PORT);
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users'); //we previously set this up in models/User.js
 
 //console.developers.google.com
 passport.use(new GoogleStrategy({
@@ -8,8 +11,6 @@ passport.use(new GoogleStrategy({
 		clientSecret : keys.googleClientSecret,
 		callbackURL : '/auth/google/callback' //this is a little different than the video
 	}, (accessToken, refreshToken, profile, done) => {
-		console.log('accessToken: ', accessToken);
-		console.log('refreshToken: ', refreshToken);
-		console.log('profile: ', profile);
+				new User({ googleId: profile.id }).save() //sets the googleId in our user Schema to the profile.id (actual Google id), save method must be called to persist it to the db
 	})
 );
