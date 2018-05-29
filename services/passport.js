c70ed83192c000fbae591859c8227580588fcb61
Madeleine Huish <madeleinehuish@gmail.com>
2018-05-29 09:08:34 -0700

    added create-react-app

--- services/passport.js
+++ services/passport.js
@@ -17,7 +17,8 @@ passport.deserializeUser((id, done) => {
 })
 
 //console.developers.google.com
-passport.use(new GoogleStrategy({
+passport.use(
+	new GoogleStrategy({
 		clientID : keys.googleClientID,
 		clientSecret : keys.googleClientSecret,
 		callbackURL : '/auth/google/callback',
